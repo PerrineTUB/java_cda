5079acf6614d02272fe2aa17bd5b08f72ce4a413
PerrineTUB <p.tubia@gmail.com>
2024-02-12 12:25:36 +0100

    Exercice 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import com.cda.classe.Exemple;
 import com.cda.classe.Habitation;
+import com.cda.classe.Vehicule;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -11,6 +12,13 @@ public class Main {
 
         Habitation maison1 = new Habitation("maison 1 ", 5, 5,2);
 
-        System.out.println("L'habitation: " + maison1.getNom() + ", à une surface de :" + maison1.surface() + "m².");
+        Vehicule voiture1 = new Vehicule("Audi", 4, 250);
+        Vehicule voiture2 = new Vehicule("Bm", 2, 250);
+
+        voiture1.Boost();
+
+        System.out.println("Le véhicule " + voiture1.getNom() + " est de type : " + voiture1.Detect());
+        System.out.println("Le véhicule " + voiture2.getNom() + " est de type : " + voiture2.Detect());
+        System.out.println("Le véhicule " + voiture1.getNom() + " possède une vitesse de : " + voiture1.getVitesse() + "Km/h");
     }
 }
--- src/com/cda/classe/Vehicule.java
+++ src/com/cda/classe/Vehicule.java
@@ -4,4 +4,49 @@ public class Vehicule {
     private String nom;
     private int nbrRoue;
     private int vitesse;
+
+    public Vehicule(){};
+    public Vehicule(String name, int nbr,int speed){
+        this.nom = name;
+        this.nbrRoue = nbr;
+        this.vitesse = speed;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getNbrRoue() {
+        return nbrRoue;
+    }
+
+    public void setNbrRoue(int nbrRoue) {
+        this.nbrRoue = nbrRoue;
+    }
+
+    public int getVitesse() {
+        return vitesse;
+    }
+
+    public void setVitesse(int vitesse) {
+        this.vitesse = vitesse;
+    }
+
+    public void Boost(){
+        this.vitesse += 50;
+    }
+
+    public String Detect(){
+        if (this.nbrRoue == 4){
+            return "voiture";
+        } else if (this.nbrRoue == 2 ){
+            return "moto";
+        } else {
+            return "Type de véhicule inconnu";
+        }
+    }
 }
